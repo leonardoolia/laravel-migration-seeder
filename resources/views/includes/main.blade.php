df29ed1f7a784d2938ed627158e7775f5dd0440b
leonardoolia <leonardooliaa@gmail.com>
2024-03-12 18:22:58 +0100

    printed table with db data

--- resources/views/includes/main.blade.php
+++ resources/views/includes/main.blade.php
@@ -3,31 +3,37 @@
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Company</th>
+      <th scope="col">Departure station</th>
+      <th scope="col">Arrival station</th>
+      <th scope="col">Departure time</th>
+      <th scope="col">Arrival time</th>
+      <th scope="col">Train code</th>
+      <th scope="col">Train carriages</th>
+      <th scope="col">In time</th>
+      <th scope="col">Cancelled</th>
     </tr>
   </thead>
+
+@forelse($trains as $train)
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+      <th scope="row">{{$train->company}}</th>
+      <td>{{$train->departure_station}}</td>
+      <td>{{$train->arrival_station}}</td>
+      <td>{{$train->departure_time}}</td>
+      <td>{{$train->arrival_time}}</td>
+      <td>{{$train->train_code}}</td>
+      <td>{{$train->train_carriages}}</td>
+      <td>{{$train->in_time}}</td>
+      <td>{{$train->cancelled}}</td>
+    </tr>   
   </tbody>
+
+@empty
+    <h2>Non ci sono treni</h2>
+
+@endforelse
 </table>
 
 @endsection
