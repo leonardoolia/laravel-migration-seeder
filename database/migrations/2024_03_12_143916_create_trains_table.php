04d1f8d0a261963cffa527e262f009899a7549a0
leonardoolia <leonardooliaa@gmail.com>
2024-03-12 15:51:29 +0100

    created db table

--- database/migrations/2024_03_12_143916_create_trains_table.php
+++ database/migrations/2024_03_12_143916_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code');
+            $table->tinyInteger('train_carriages')->unsigned();
+            $table->boolean('in_time');
+            $table->boolean('cancelled');
             $table->timestamps();
         });
     }
