4ada7d41e1ed161038a3a42f10fc3637f4c4338b
leonardoolia <leonardooliaa@gmail.com>
2024-03-12 15:37:56 +0100

    created layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,15 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Laravel</title>
+    <title>{{env('APP_NAME', 'Laravel Trains')}}</title>
 
 </head>
+
+<body>
+    @extends('layouts.main')
+    @section('main-content')
+        @include('includes.main')
+    @endsection
+</body>
 
 </html>
